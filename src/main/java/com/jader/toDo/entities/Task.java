92f1865d7410cd8d493802f6a6c841d4216fa664
jaderAdriel <jaderadriel7@gmail.com>
2025-01-09 19:13:31 -0300

    Add setters for user and id in Task entity
    Included new setter methods for the `user` and `id` fields to enhance the flexibility of the `Task` entity. These additions enable modification of these fields after object creation, supporting better usability and more diverse use cases.

--- src/main/java/com/jader/toDo/entities/Task.java
+++ src/main/java/com/jader/toDo/entities/Task.java
@@ -80,6 +80,14 @@ public class Task {
         return user;
     }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
