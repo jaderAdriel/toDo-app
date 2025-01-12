cc00d49e9c576b9ed8b036761957453b3c430b72
jaderAdriel <jaderadriel7@gmail.com>
2025-01-11 23:13:29 -0300

    formating code

--- src/main/java/com/jader/toDo/entities/Task.java
+++ src/main/java/com/jader/toDo/entities/Task.java
@@ -8,15 +8,22 @@ import java.util.Objects;
 
 @Entity
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     private String description;
+
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TaskStatus status;
+
     private LocalDateTime creationDate;
+
     private LocalDateTime deadline;
 
     @ManyToOne
